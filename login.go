804b3bc1c03af01b26b0bbfc332b473a76820079
rkfg <rkfg@rkfg.me>
2023-07-30 17:46:37 +0300

    Initial commit

---
+++ login.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"bufio"
+	"embed"
+	"fmt"
+	"html/template"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+//go:embed templates/*
+var templates embed.FS
+
+var tpl = template.Must(template.ParseFS(templates, "templates/*"))
+
+var creds = map[string]string{}
+
+type Result map[string]interface{}
+
+func JSONError(c echo.Context, code int, err error) error {
+	return JSONErrorMessage(c, code, err.Error())
+}
+
+// JSONErrorMessage returns a JSON error with the provided http error code and message
+func JSONErrorMessage(c echo.Context, code int, msg string) error {
+	return c.JSON(code, Result{"message": msg})
+}
+
+func loginPageHandler(c echo.Context) error {
+	return tpl.Execute(c.Response(), struct{}{})
+}
+
+func failLogin(c echo.Context) error {
+	return c.Redirect(302, "/login")
+}
+
+func setToken(c echo.Context, subject string) error {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{ExpiresAt: jwt.NewNumericDate(time.Now().AddDate(0, 0, 7)), Subject: subject})
+	signed, err := token.SignedString([]byte(params.JWTSecret))
+	if err != nil {
+		return err
+	}
+	c.SetCookie(&http.Cookie{Name: "sdkey", Value: signed, HttpOnly: true, SameSite: http.SameSiteLaxMode})
+	return nil
+}
+
+func loginHandler(c echo.Context) error {
+	login := strings.ToLower(c.FormValue("login"))
+	password := c.FormValue("password")
+	if login == "" {
+		return failLogin(c)
+	}
+	passwordHashed, ok := creds[login]
+	if !ok {
+		return failLogin(c)
+	}
+	if bcrypt.CompareHashAndPassword([]byte(passwordHashed), []byte(password)) != nil {
+		return failLogin(c)
+	}
+	err := setToken(c, login)
+	if err != nil {
+		return JSONError(c, 400, err)
+	}
+	return c.Redirect(302, "/")
+}
+
+func loadCreds(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := s.Text()
+		if params.JWTSecret == "" {
+			params.JWTSecret = line
+			continue
+		}
+		split := strings.Split(line, ":")
+		if len(split) != 2 {
+			log.Printf("invalid cred line: %s", line)
+			continue
+		}
+		login := split[0]
+		pwd := split[1]
+		creds[login] = pwd
+	}
+	return nil
+}
+
+func saveCreds(filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	f.WriteString(params.JWTSecret + "\n")
+	for u, p := range creds {
+		_, err := f.WriteString(fmt.Sprintf("%s:%s\n", u, p))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func randomString(r *rand.Rand, length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letterBytes[r.Int63()%int64(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func keyErrorHandler(c echo.Context, err error) error {
+	log.Print(err)
+	return c.Redirect(302, "/login")
+}
+
+func renewHandler(c echo.Context) error {
+	token := c.Get("user").(*jwt.Token)
+	if token == nil {
+		return fmt.Errorf("no token")
+	}
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return JSONError(c, 400, err)
+	}
+	if subject == "" {
+		return JSONErrorMessage(c, 400, "user not set")
+	}
+	err = setToken(c, subject)
+	if err != nil {
+		return JSONError(c, 400, err)
+	}
+	log.Printf("Token renewed for user %s", subject)
+	return c.Redirect(302, "/")
+}
+
+func earlyCheckMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if c.Request().URL != nil && c.Request().URL.Path == "/" {
+				token := c.Get("user").(*jwt.Token)
+				if token != nil {
+					date, err := token.Claims.GetExpirationTime()
+					if err != nil {
+						return JSONError(c, 400, err)
+					}
+					if time.Until(date.Time) < time.Minute*3 {
+						c.Redirect(302, "/renew")
+					}
+				}
+			}
+			return next(c)
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net/url"
+	"time"
+
+	"github.com/btcsuite/go-flags"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var params struct {
+	CredFilename string `short:"f" description:"Credentials filename" required:"true"`
+	AddUser      bool   `short:"a" description:"Add new user"`
+	Username     string `short:"u" description:"Username for -a"`
+	Password     string `short:"p" description:"Password for -a"`
+	TargetURL    string `short:"t" description:"Target URL to proxy to"`
+	Address      string `short:"l" description:"Listen at this address" default:"0.0.0.0:8000"`
+	JWTSecret    string
+}
+
+func main() {
+	_, err := flags.Parse(&params)
+	if err != nil {
+		return
+	}
+	if params.AddUser {
+		if params.Username == "" || params.Password == "" {
+			log.Fatal("Specify username and password to add")
+		}
+		hashed, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcrypt.DefaultCost)
+		if err != nil {
+			log.Fatalf("Error hashing password: %s", err)
+		}
+		err = loadCreds(params.CredFilename)
+		if err != nil {
+			log.Printf("Error loading existing users: %s, will create a new file and JWT secret", err)
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			params.JWTSecret = randomString(r, 64)
+		}
+		creds[params.Username] = string(hashed)
+		saveCreds(params.CredFilename)
+		log.Printf("User %s added", params.Username)
+		return
+	}
+	if params.TargetURL == "" {
+		log.Fatal("Specify the target URL to proxy requests to (-t http://127.0.0.1...)")
+	}
+	err = loadCreds(params.CredFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	e := echo.New()
+	// e.Any("/*", proxy,
+	// middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{KeyLookup:
+	// "cookie:sdkey", Validator: validateKey, ErrorHandler: keyErrorHandler}))
+	e.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:   []byte(params.JWTSecret),
+		ErrorHandler: keyErrorHandler,
+		TokenLookup:  "cookie:sdkey",
+		Skipper: func(c echo.Context) bool {
+			return c.Path() == "/login"
+		},
+	}))
+	e.GET("/login", loginPageHandler)
+	e.POST("/login", loginHandler)
+	tgturl, err := url.Parse(params.TargetURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	e.GET("/renew", renewHandler)
+	e.Group("/*", earlyCheckMiddleware(), middleware.Proxy(middleware.NewRoundRobinBalancer([]*middleware.ProxyTarget{{URL: tgturl}})))
+	e.Start(params.Address)
+}
